c7beaa9c9de8a0a36dbb0aeedef16594129ff86e
lucky <luckyes144@gmail.com>
2026-09-04 21:11:38 +0600

    Using the Cart Context & Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./component/Header";
 import Meals from "./component/Meals";
+import { CartContextProvider } from "./store/CartContext.jsx";
+
+
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
      <Header />
      <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
