34901e5f187feaaeeaea932ad641d65b515b0028
lucky <luckyes144@gmail.com>
2026-09-05 20:34:46 +0600

    Working on the Cart ItemsWorking on the Cart Items

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import Cart from "./component/Cart.jsx";
 import Header from "./component/Header";
 import Meals from "./component/Meals";
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 
 
 
 function App() {
   return (
-    <CartContextProvider>
-     <Header />
-     <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
