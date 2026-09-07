b38ccf670a6aaa0dcac0bae4f8058601d78991fb
lucky <luckyes144@gmail.com>
2026-09-07 20:38:28 +0600

    Adding a Custom Input Component & Managing Modal Visibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Cart from "./component/Cart.jsx";
+import Checkout from "./component/Checkout.jsx";
 import Header from "./component/Header";
 import Meals from "./component/Meals";
 import { CartContextProvider } from "./store/CartContext.jsx";
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Meals />
         <Cart />
+        <Checkout />
       </CartContextProvider>
     </UserProgressContextProvider>
   );
